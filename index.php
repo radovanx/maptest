bf0e25f54365905b21b2803e09f3ea7f69b61e59
Radomir Bednar <radovanmail@gmail.com>
2014-03-02 14:33:44 +0100

    new commit

--- index.php
+++ index.php
@@ -1,33 +1,11 @@
-<!DOCTYPE html> 
-<html> 
-	<head>
-  <meta charset="UTF-8">	
-	<title>tester Picker</title> 
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-	<link rel="stylesheet" href="jquery.mobile.structure-1.0.1.css" />
-	<link rel="apple-touch-icon" href="images/launch_icon_57.png" />
-	<link rel="apple-touch-icon" sizes="72x72" href="images/launch_icon_72.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="images/launch_icon_114.png" />
-	<link rel="stylesheet" href="jquery.mobile-1.0.1.css" />
-	<link rel="stylesheet" href="custom.css" />
- 
-	<script src="js/jquery-1.7.1.min.js"></script>
-	<script src="js/jquery.mobile-1.0.1.min.js"></script>
-     
-</head> 
-
-<body> 
-<div data-role="page" id="home" data-theme="c">
-
-	<div data-role="content">
-	<div id="branding">
-	<h1>Tester Picker </h1>
-	</div>
-	<div class="choice_list"> 
-	<h1>Category</h1>
-	
-        
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+       
        
         
         <script> 
